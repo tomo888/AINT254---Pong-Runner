ed63639b526fa3f6a182ad5b37d8d9fa5dad7c00
tomo888 <33130687+tomo888@users.noreply.github.com>
2019-11-07 15:59:14 +0000

    v0.21
    Added framework for powerups system; editing paddle size, destroying gameobjects. Begun work on slow down time powerup but unfinished due to technical difficulties.

---
+++ PongRunner/Assets/Scripts/ExpandPaddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandPaddle : MonoBehaviour
+{
+    public float multiplier = 2.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            onPickup(GameObject.Find("Paddle"));
+        }
+    }
+
+    void onPickup(GameObject gameObject)
+    {
+        gameObject.transform.localScale *= multiplier;
+        destroy();
+    }
+
+    void destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ PongRunner/Assets/Scripts/SlowTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTime : MonoBehaviour
+{
+    public bool upgrade = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            upgrade = true;
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (upgrade == true)
+        {
+            Time.timeScale = 0.5f;
+            System.Threading.Thread.Sleep(5000);
+            Time.timeScale = 1.0f;
+        }
+
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+
+    }
+
+    void destroy()
+    {
+    }
+}
