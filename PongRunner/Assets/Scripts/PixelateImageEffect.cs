b9a13d481964ca6379af313efadd388cd3ac71a8
tomo888 <33130687+tomo888@users.noreply.github.com>
2019-12-24 15:19:39 +0000

    v0.61
    Implemented pixelated 16-bit graphical shader; added audio files, to be implemented into scenes; updated credits txt file.

---
+++ PongRunner/Assets/Scripts/PixelateImageEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] //for debugging purposes
+public class PixelateImageEffect : MonoBehaviour
+{
+
+    public Material effectMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, effectMaterial);
+    }
+}
