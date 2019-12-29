9f8339b2011326e517c675380c504de1b8f34006
tomo888 <33130687+tomo888@users.noreply.github.com>
2019-12-29 18:57:31 +0000

    v0.8
    Finished main menu UI; added pause menu to gameplay, with script that scales down music volume as well as adds a small graphical filter that dims the background behind the menu; fixed UI scaling issues.

---
+++ PongRunner/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseUI;
+    public AudioSource gameST;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameST.volume *= 0.25f;
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameST.volume = 0.4f;
+        gamePaused = false;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
